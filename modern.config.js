43744607ab520e5a0819136e6cdcdb5a397f9cc0
Max 🕺 Coplan <max.coplan@bytedance.com>
2025-05-13 15:31:01 -0700

    repro bug

---
+++ modern.config.js
@@ -0,0 +1,25 @@
+import { appTools, defineConfig } from '@modern-js/app-tools';
+
+// https://modernjs.dev/en/configure/app/usage
+export default defineConfig({
+  runtime: {
+    router: true,
+  },
+  server: {
+    ssr: {
+      mode: 'string',
+      loaderFailureMode: 'clientRender',
+    },
+  },
+  output: {
+    polyfill: 'usage',
+    distPath: {
+      server: 'server'
+    },
+  },
+  plugins: [
+    appTools({
+      bundler: 'rspack', // Set to 'webpack' to enable webpack
+    }),
+  ],
+});
